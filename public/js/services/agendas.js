71bc93960a5b8f859ea287a0df49085e488313b8
miriam <miemanuel1@gmail.com>
2014-01-23 15:13:46 +0200

    agendas controller

--- public/js/services/agendas.js
+++ public/js/services/agendas.js
@@ -1,14 +1,24 @@
-//Global service for global variables
-angular.module('mean').factory("Agendas", ['$http',
-    function($http) {
-        var _this = this,
-            getAgendas = function(cb) {
-                $http.get("/agendas", {}).success(function(data) {
-                    cb(data);
-                });
-            };
-        return {
-            getAgendas: getAgendas
-        };
+// angular.module('mean').factory("Agendas", ['$http',
+//     function($http) {
+//         var _this = this,
+//             getAgendas = function(cb) {
+//                 $http.get("/agendas", {}).success(function(data) {
+//                     cb(data);
+//                 });
+//             };
+//         return {
+//             getAgendas: getAgendas
+//         };
+//     }
+// ]);
+
+
+
+//Agendas service used for agendas REST endpoint
+angular.module('mean').factory("Agendas", ['$resource', 
+    function($resource) {
+        return $resource('agendass/:agendaId', {
+            agendaId: '@_id'
+        });
     }
 ]);
